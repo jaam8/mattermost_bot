20bf282600b0047cda22921aa573c9135128e52d
jaam8 <jaam8@yandex.ru>
2025-03-31 04:28:58 +0700

    fix: fixed incorrect selection caused by index

--- tarantool/init.lua
+++ tarantool/init.lua
@@ -14,29 +14,37 @@ box.once('bootstrap_schema', function()
             {name = 'id',         type = 'string'},
             {name = 'question',   type = 'string'},
             {name = 'options',    type = 'array'},
-            {name = 'votes',      type = 'map'},
+            {name = 'votes',      type = 'string'},
             {name = 'creator_id', type = 'string'},
-            {name = 'active',     type = 'boolean'},
+            {name = 'is_active',  type = 'boolean'},
         }
     })
     polls_space:create_index('primary', {
         if_not_exists = true,
-        type = 'hash',
+        type = 'tree',
         parts = {1, 'string'}
     })
 
     local votes_space = box.schema.space.create('votes', {
     if_not_exists = true,
     format = {
-        {name = 'poll_id', type = 'string'},
-        {name = 'user_id', type = 'string'},
-        {name = 'choice', type = 'integer'}
+        {name = 'poll_id',   type = 'string'},
+        {name = 'user_id',   type = 'string'},
+        {name = 'choice_id', type = 'string'}
     }})
 
+    votes_space:create_index('primary', {
+        if_not_exists = true,
+        type = 'hash',
+        parts = {'poll_id', 'user_id'}
+    })
+    votes_space:create_index('user_poll', {
+        if_not_exists = true,
+        type = 'hash',
+        parts = {1, 'string', 2, 'string'}
+    })
 end)
 
 local log = require('log').new(app_name)
 log.info('loaded')
-print("Tarantool is up and running!")
-local result = box.space.polls:select({}, {limit = 10})
-print(result)
+log.info("Tarantool is up and running!")
